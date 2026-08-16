7f424869ce2c6d04b032c103fb7ea339f4879771
kimc19770525-ux <kimc19770525@gmail.com>
2026-08-17 08:59:36 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // exceljs가 참조하는 Node 전용 모듈을 브라우저에서 무력화
+      fs: 'exceljs/dist/es5/browser',
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
 })
